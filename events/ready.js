76eb8f29649bc6dfe4f6495a1417b0df1d8d3978
Ap4cheSK <apache@atlas.sk>
2023-11-22 10:01:24 +0100

    added dobre rejnou

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,5 @@
 const client = require("../");
+const cron = require("node-cron");
 
 client.once("ready", () => {
 	const activities = [
@@ -7,6 +8,14 @@ client.once("ready", () => {
 
 	client.user.setActivity(activities[0]);
 	client.user.setStatus("online");
-
 	console.log("\nBot Ready!\n=========");
+
+	// ************ //
+	// DOBRE REJNOU //
+	// ************ //
+	cron.schedule("0 0 8 * * *", () => {
+		const guild = client.guilds.cache.get(process.env.GUILD_ID);
+		const channel = guild.channels.cache.get(process.env.CHAT_ID);
+		channel.send("https://cdn.discordapp.com/attachments/1033122941719822377/1138344315966464030/dobre_rejnou.mp4");
+	});
 });
