230f45102a9bb5fe7b6affd2d294d33c8e6b8dd9
Ap4cheSK <apache@atlas.sk>
2023-11-22 10:27:36 +0100

    instagram fixer

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -9,7 +9,13 @@ client.on("messageCreate", (message) => {
 	if(twitterRegex.test(message.content)) {
 		let twitterPostLink = message.content.match(twitterRegex)[0];
 		twitterPostLink = twitterPostLink.replace("twitter.com/", "fxtwitter.com/");
-
 		message.reply(`I have fixed the Twitter for you: ${twitterPostLink}`);
+	}
+
+	const instagramRegex = /^https:\/\/www\.instagram\.com\/reel\/[a-zA-Z0-9_-]+\/$/;
+	if(instagramRegex.test(message.content)) {
+		let instagramPostLink = message.content.match(instagramRegex)[0];
+		instagramPostLink = instagramPostLink.replace("instagram.com/", "ddinstagram.com/");
+		message.reply(`I have fixed the Instagram for you: ${instagramPostLink}`);
 	}
 });
--- events/ready.js
+++ events/ready.js
@@ -10,12 +10,14 @@ client.once("ready", () => {
 	client.user.setStatus("online");
 	console.log("\nBot Ready!\n=========");
 
+	const guild = client.guilds.cache.get(process.env.GUILD_ID);
+	const channel = guild.channels.cache.get(process.env.CHAT_ID); //CHAT CHANNEL
+	channel.send("https://cdn.discordapp.com/attachments/1033122941719822377/1138344315966464030/dobre_rejnou.mp4");
+
 	// ************ //
 	// DOBRE REJNOU //
 	// ************ //
 	cron.schedule("0 0 8 * * *", () => {
-		const guild = client.guilds.cache.get(process.env.GUILD_ID);
-		const channel = guild.channels.cache.get(process.env.CHAT_ID);
 		channel.send("https://cdn.discordapp.com/attachments/1033122941719822377/1138344315966464030/dobre_rejnou.mp4");
 	});
 });
