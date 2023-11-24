9337a8de020bf70ff419b89579329066e4eca269
Ap4cheSK <apache@atlas.sk>
2023-11-24 10:34:46 +0100

    added xcom link fixer

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -11,6 +11,13 @@ client.on("messageCreate", (message) => {
 		twitterPostLink = twitterPostLink.replace("twitter.com/", "fxtwitter.com/");
 		message.reply(`I have fixed the Twitter for you: ${twitterPostLink}`);
 	}
+	
+	const xcomRegex = /https:\/\/x\.com\/\S+\/status\/\d+/;
+	if(xcomRegex.test(message.content)) {
+		let xcomPostLink = message.content.match(xcomRegex)[0];
+		xcomPostLink = xcomPostLink.replace("x.com/", "fxtwitter.com/");
+		message.reply(`I have fixed the X for you: ${xcomPostLink}`);
+	}
 
 	const instagramRegex = /^https:\/\/www\.instagram\.com\/reel\/[a-zA-Z0-9_-]+\/$/;
 	if(instagramRegex.test(message.content)) {
